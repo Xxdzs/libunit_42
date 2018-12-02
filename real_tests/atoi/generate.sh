1d908799a7828407406f06f48bc92495000a9143
Antoine GAGNIERE <sid.xxdzs@gmail.com>
2018-12-02 14:03:43 +0100

    Shortened a test

--- real_tests/atoi/generate.sh
+++ real_tests/atoi/generate.sh
@@ -6,7 +6,7 @@
 #    By: angagnie <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/12/01 22:42:13 by angagnie          #+#    #+#              #
-#    Updated: 2018/12/02 13:57:12 by angagnie         ###   ########.fr        #
+#    Updated: 2018/12/02 14:01:07 by angagnie         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -45,7 +45,7 @@ echo "\n\ttest_list[0] = NEW_ARRAY(t_test);" >> $launcher
 count=1
 for input in "basic|28" "negative|-8128" "empty|" "negative_zero|-0" "space| 496" "plus_sign|+1729 Ramanujan" "tab| \\\t 33550336 Perfect" \
 	"carriage_return| \\\r +877 Bell Prime" "form_feed| \\\f 16127 Carol Prime" "vertical_tab| \\\v 7057 Cuban Prime" "two_plus_signs|++3" \
-	"invalid_first_char|_197 Chen" "leading_zeros| 000231" "combo|\\\r \\\v \\\n -0000000987654321" \
+	"invalid_first_char|_197 Chen" "leading_zeros| 000231" "combo|\\\r\\\v\\\n-000987654321" \
 	"int_min|-2147483648" "int_max|2147483647"
 do
 	name=${input%%|*}_test
