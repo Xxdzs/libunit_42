6640a548b3ea686b7226ca5bd88b88a264e3225c
Antoine GAGNIERE <sid.xxdzs@gmail.com>
2018-12-02 15:33:44 +0100

    Framework Compiles

--- framework/libunit.c
+++ framework/libunit.c
@@ -6,11 +6,12 @@
 /*   By: angagnie <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/01 21:39:01 by angagnie          #+#    #+#             */
-/*   Updated: 2018/12/02 15:00:18 by angagnie         ###   ########.fr       */
+/*   Updated: 2018/12/02 15:21:53 by angagnie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libunit.h"
+#include "ft_printf.h"
 #include "ft_color.h"
 #include <signal.h>
 
@@ -18,7 +19,9 @@
 
 int				load_test(t_array *list, const char *name, t_fptr function)
 {
-	fta_push_back(list, NEW_TEST(name, function));
+	const t_test	test = NEW_TEST(name, function);
+	fta_append(list, &test, 1);
+	return (0);
 }
 
 static unsigned	run_test(t_test *test)
@@ -32,14 +35,14 @@ static unsigned	run_test(t_test *test)
 		wait(&status);
 		ft_printf("[%s ", test->name);
 		if (status == 0)
-			ft_printf("%sOK%s", COLOR(GREEN), EOC);
+			ft_printf("%sOK", COLOR(GREEN));
 		else if (status == SIGBUS)
-			ft_printf("%sBUSE%s", COLOR(BOLD, RED), EOC);
+			ft_printf("%sBUSE", COLOR(BOLD, RED));
 		else if (status == SIGSEGV)
-			ft_printf("%sSIGV%s", COLOR(BOLD, RED), EOC);
+			ft_printf("%sSIGV", COLOR(BOLD, RED));
 		else
-			ft_printf("%sKO%s", COLOR(RED), EOC);
-		ft_printf("]");
+			ft_printf("%sKO", COLOR(RED));
+		ft_printf("%s]", EOC);
 	}
 	return (status == 0);
 }
@@ -50,8 +53,8 @@ int				launch_tests(const char *name, t_array *list)
 	unsigned	success = 0;
 
 	ft_printf("%s : ", name);
-	while (ARRAY_HASNEXT(iterator))
+	while (ARRAY_HASNEXT(list, iterator))
 		success += run_test((t_test*)iterator);	
-	ft_printf("\n%u / %u tests passed\n", success, fta_size(list));
-	return (-(success != fta_size(list)));
+	ft_printf("\n%u / %u tests passed\n", success, list->size);
+	return (-(success != list->size));
 }
--- framework/libunit.h
+++ framework/libunit.h
@@ -3,9 +3,9 @@
 
 # include "ft_array.h"
 
-typedef int (*)(void)	t_fptr;
+typedef int				(*t_fptr)(void);
 
-typedef s_test			t_test;
+typedef struct s_test	t_test;
 
 struct					s_test
 {
@@ -16,6 +16,6 @@ struct					s_test
 # define NEW_TEST(NAME, FUNCTION) (t_test){NAME, FUNCTION}
 
 int						load_test(t_array *list, const char *name, t_fptr function);
-int						launch_tests(t_array *list);
+int						launch_tests(const char *name, t_array *list);
 
 #endif
