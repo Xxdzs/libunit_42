7c84b2d29938d518de24c61d5bf63339993dda70
Antoine GAGNIERE <sid.xxdzs@gmail.com>
2018-12-02 13:45:19 +0100

    Fixed launcher generation

--- real_tests/atoi/generate.sh
+++ real_tests/atoi/generate.sh
@@ -6,7 +6,7 @@
 #    By: angagnie <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2018/12/01 22:42:13 by angagnie          #+#    #+#              #
-#    Updated: 2018/12/02 13:34:24 by angagnie         ###   ########.fr        #
+#    Updated: 2018/12/02 13:44:47 by angagnie         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -23,13 +23,13 @@ echo "# define $(echo $header | tr '[a-z].' '[A-Z]_')\n" >> $header
 echo '#include "libunit.h"' > $launcher
 echo "#include \"$header\"\n" >> $launcher
 echo "int\t\tatoi_launcher(void)\n{" >> $launcher
-echo "\tt_array\ttest_list[1];\n" >> $launcher
-echo "\n\ntest_list[0] = NEW_ARRAY(t_test);" >> $launcher
+echo "\tt_array\ttest_list[1];" >> $launcher
+echo "\n\ttest_list[0] = NEW_ARRAY(t_test);" >> $launcher
 
 count=1
 for input in "basic|28" "negative|-8128" "empty|" "negative_zero|-0" "space| 496" "plus_sign|+1729 Ramanujan" "tab| \\\t 33550336 Perfect" \
 	"carriage_return| \\\r +877 Bell Prime" "form_feed| \\\f 16127 Carol Prime" "vertical_tab| \\\v 7057 Cuban Prime" "two_plus_signs|++3" \
-	"invalid_first_character|_197 Chen" "leading_zeros| 000000000002311 Euclid Prime" "combo|\\\r \\\v \\\n -0000000987654321" \
+	"invalid_first_char|_197 Chen" "leading_zeros| 000000000002311 Euclid Prime" "combo|\\\r \\\v \\\n -0000000987654321" \
 	"int_min|-2147483648" "int_max|2147483647"
 do
 	name=${input%%|*}_test
